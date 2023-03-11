36550b97ca8e7cfd835a73360baf3911f15f50f4
Carlos Gamboa <gamboamc@outlook.com>
2023-03-10 20:24:47 -0500

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "standard-with-typescript",
+    "eslint-config-prettier",
+    "prettier",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+    "import/resolver": {
+      node: {
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
+  overrides: [],
+  parserOptions: {
+    ecamFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+    parser: "@typescript-eslint/parser",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": "off",
+  },
+};
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App(): JSX.Element {
+  return (
+    <div>
+      <h1>Online photo exhibition</h1>
+    </div>
+  );
+}
+
+export default App;
